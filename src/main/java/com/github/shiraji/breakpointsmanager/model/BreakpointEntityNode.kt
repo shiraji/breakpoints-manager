104e544fce4b4c7edd68f0771ba403a6ff6b4e74
shiraji <isogai.shiraji@gmail.com>
2016-06-08 14:13:03 +0900

    Rename custom node class

--- src/main/java/com/github/shiraji/breakpointsmanager/model/BreakpointEntityNode.kt
+++ src/main/java/com/github/shiraji/breakpointsmanager/model/BreakpointEntityNode.kt
@@ -2,7 +2,7 @@ package com.github.shiraji.breakpointsmanager.model
 
 import java.io.File
 
-class BreakpointNodeEntity(val entity: BreakpointEntity) {
+class BreakpointEntityNode(val entity: BreakpointEntity) {
     override fun toString(): String {
         return "Line ${entity.line} in ${entity.fileUrl.substringAfterLast(File.separator)}"
     }
